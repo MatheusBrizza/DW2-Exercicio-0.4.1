ad2941a6ea85d8401f3964ba32508c1385b8ea75
MatheusBrizza <matheusbj95@gmail.com>
2022-05-20 22:41:33 -0300

    Exercício concluído em index.js e enunciado adicionado em README.md

--- README.md
+++ README.md
@@ -1,3 +1,29 @@
 # node-guzbmt
 
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-guzbmt)
+Para cada aluno, gere a média geral obtida de todas as notas. Você pode salvar esse valor numa propriedade chamada `mediaGeral`. Você pode usar o `map` ou o `forEach` para esse exercício.
+
+```jsx
+const alunos = [
+  {
+    nome: 'Mathias',
+    notas: [
+      { cadeira: 'DW2', nota: 10 },
+      { cadeira: 'BD2', nota: 7 },
+    ],
+  },
+  {
+    nome: 'Luiza',
+    notas: [
+      { cadeira: 'DW2', nota: 8 },
+      { cadeira: 'BD2', nota: 8 },
+    ],
+  },
+  {
+    nome: 'Pedro',
+    notas: [
+      { cadeira: 'DW2', nota: 10 },
+      { cadeira: 'BD2', nota: 10 },
+    ],
+  }
+]
+```
--- index.js
+++ index.js
@@ -1,3 +1,35 @@
 // run `node index.js` in the terminal
 
+const alunos = [
+  {
+    nome: 'Mathias',
+    notas: [
+      { cadeira: 'DW2', nota: 10 },
+      { cadeira: 'BD2', nota: 7 },
+    ],
+  },
+  {
+    nome: 'Luiza',
+    notas: [
+      { cadeira: 'DW2', nota: 8 },
+      { cadeira: 'BD2', nota: 8 },
+    ],
+  },
+  {
+    nome: 'Pedro',
+    notas: [
+      { cadeira: 'DW2', nota: 10 },
+      { cadeira: 'BD2', nota: 10 },
+    ],
+  },
+];
 
+alunos.forEach((aluno) => {
+  const somaNotas = aluno.notas.reduce((acumulador, proximoValor) => {
+    return acumulador + proximoValor.nota;
+  }, 0);
+  console.log(aluno.notas);
+  aluno.mediaGeral = somaNotas / aluno.notas.length;
+});
+
+console.log(alunos);
