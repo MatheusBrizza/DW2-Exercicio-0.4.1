fed92059957f164a45f627a3017095522363723f
MatheusBrizza <matheusbj95@gmail.com>
2022-05-27 19:43:17 -0300

    correçãodo exercício feito com forOf e ForEach

--- index.js
+++ index.js
@@ -28,8 +28,39 @@ alunos.forEach((aluno) => {
   const somaNotas = aluno.notas.reduce((acumulador, proximoValor) => {
     return acumulador + proximoValor.nota;
   }, 0);
-  console.log(aluno.notas);
+  //console.log(aluno.notas);
   aluno.mediaGeral = somaNotas / aluno.notas.length;
 });
 
 console.log(alunos);
+
+// correção com forOf;
+
+function gerarMediaGeral(notas) {
+  let mediaGeral = 0;
+  for (let aluno of alunos) {
+    mediaGeral = mediaGeral + aluno.nota;
+    mediaGeral = mediaGeral + nota.nota;
+  }
+  return mediaGeral / notas.length;
+}
+
+// correção com forEach
+
+function gerarMediaGeral2(notas) {
+  let mediaGeral = 0;
+  notas.forEach((nota) => {
+       mediaGeral = mediaGeral + nota.nota;
+  });
+  return mediaGeral / notas.length;
+}
+
+const alunoComMediaGeral = alunos.map((aluno) => {
+  const mediaGeral = gerarMediaGeral2(aluno.notas);
+  aluno.mediaGeral = mediaGeral;
+});
+
+//const mediaGeral = gerarMediaGeral(alunos[1].notas);
+
+console.log(mediaGeral);
+
